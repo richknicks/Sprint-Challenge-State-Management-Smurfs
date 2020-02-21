64983206422b2cc8e0367cfe6b01fa0cd9df178a
richknicks <rlynch@gmail.com>
2020-02-21 12:17:01 -0500

    app created

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,51 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-class App extends Component {
-  render() {
+import axios from 'axios';
+import { FormContext } from '../contexts/FormContext';
+import { SmurfCardContext } from '../contexts/SmurfCardContext';
+
+// Components
+import { Form } from './Form';
+import { SmurfCard } from './SmurfCard';
+
+const App = ()=> {
+  const [smurf, setSmurf] = useState([]);
+  const [newSmurf, setNewSmurf]=useState({name:"", age:"", height:""});
+  
+  useEffect(()=>{
+    axios
+    .get('http://localhost:3333/smurfs')
+    .then(response => {
+      console.log("This is get",response.data)
+      setSmurf(response.data)
+    })
+    .catch(error => console.log('There is an error', error))
+
+  },[])
+  const Add = (item)=>{
+    axios
+    .post('http://localhost:3333/smurfs',item)
+    .then(response =>{
+      console.log("This is post",response.data)
+      setNewSmurf(response.data)
+    })
+    .catch(error => console.log("Not working",error))
+  }
+
+  
+ 
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        
+        <SmurfCardContext.Provider value={{smurf}}>
+        <FormContext.Provider value={{newSmurf,smurf,setNewSmurf, Add}}>  
+        <Form/>
+        <SmurfCard/>
+        </FormContext.Provider>
+        </SmurfCardContext.Provider>
       </div>
     );
   }
-}
+
 
 export default App;
---
+++ smurfs/src/contexts/FormContext.js
@@ -0,0 +1,3 @@
+import { createContext } from React;
+
+export const FormContext = createContext();
